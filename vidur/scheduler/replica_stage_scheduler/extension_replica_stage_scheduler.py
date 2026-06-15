f272b6ee275794ce878dead28cd9f8a243bfce21
vighnesh-shivaraju <vighneshshivaraju2985@gmail.com>
2026-06-15 20:40:02 +0530

    Update extension_replica_stage_scheduler.py

--- vidur/scheduler/replica_stage_scheduler/extension_replica_stage_scheduler.py
+++ vidur/scheduler/replica_stage_scheduler/extension_replica_stage_scheduler.py
@@ -4,19 +4,19 @@ def run_iteration(self, batch):
     K = self.config.speculative_window
 
     #time taken by draft model to guess k tokens
-    draft_latency = self.draft_model_estimator.get_runtime(batch, num_tokens=K)
+    draft_time = self.draft_model_estimator.get_runtime(batch, num_tokens=K)
 
     #training all tokens in parallel rather than doing in individual steps
-    verify_latency = self.runtime_estimator.get_runtime(batch, num_tokens=K, is_verify=True)
+    time_verification = self.runtime_estimator.get_runtime(batch, num_tokens=K, is_verify=True)
 
     #determining actual number of accpeted tokens
-    accepted_count = self.simulation_policy.get_accepted_tokens(batch, K)
+    count_ok = self.simulation_policy.get_accepted_tokens(batch, K)
 
     #updating state with multiple tokens by adding multiple tokens
     for request in batch.requests:
-        request.on_tokens_generated(accepted_count)
+        request.on_tokens_generated(count_ok)
 
     #total time is being made sum of time taken by draft and target model
-    total_latency = draft_latency + verify_latency
-    self.metrics_tracker.on_iteration_complete(total_latency)
-    return total_latency
+    total_time = draft_time + time_verification
+    self.metrics_tracker.on_iteration_complete(total_time)
+    return total_time
